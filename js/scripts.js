666625dec3e828bf14518d8834c25520647bfc8a
Bradbuchholz <bradbuchholz@gmail.com>
2022-04-22 17:38:40 -0700

    function 1 broken, debugging

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,25 @@
 
 function beepBoop(text) {
   const parsed = parseInt(text);
-  return parsed;
-  const array = parsed;
+  let array = [Number];
+  array.push(text);
+}
+// function 2 
+
+
+
+
+function thisSucks(text) {
+  const parsed = parseInt(text);
+  const array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+  numberArray.slice[text];
+  return array;
 
 }
-
-// function 2 
+// closest yet, needs loop
+function thisIsGreat(text) {
+  const parsed = parseInt(text);
+  let array = [];
+  array.push(text);
+  return array;
+}
