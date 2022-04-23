316616b7d1b44b711df6d2abeda76d3b3ab07418
Bradbuchholz <bradbuchholz@gmail.com>
2022-04-22 22:23:20 -0700

    Fix beepBoop function

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,10 @@
 // function 1 const array(user input)
 
 function beepBoop(text) {
-  const parsed = parseInt(text);
-  let array = [Number];
-  array.push(text);
-}
-// function 2 
-
-
-
-
-function thisSucks(text) {
-  const parsed = parseInt(text);
-  const array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
-  numberArray.slice[text];
-  return array;
-
-}
-// closest yet, needs loop
-function thisIsGreat(text) {
-  const parsed = parseInt(text);
-  let array = [];
-  array.push(text);
-  return array;
+  const userNumber = parseInt(text);
+  const finalArray = [];
+  for (i = 0; i <= userNumber; i++) {
+    finalArray.push(i);
+    }
+  return finalArray; 
 }
