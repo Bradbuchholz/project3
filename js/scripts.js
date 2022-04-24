f1b6bc702ce5db576eaf1682f1cb117bdf563586
Bradbuchholz <bradbuchholz@gmail.com>
2022-04-23 18:13:10 -0700

    Add const numbers

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
+const num1 = 1;
+const num2 = 2;
+const num3 = 3;
+
+
 // function 1 const array(user input)
 
 function beepBoop(text) {
@@ -7,4 +12,10 @@ function beepBoop(text) {
     finalArray.push(i);
     }
   return finalArray; 
+}
+
+// Test: "It should replace all digits in a number containing 1 with "Beep!""
+
+function beep(finalArray) {
+  
 }
